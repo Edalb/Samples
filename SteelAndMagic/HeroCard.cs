d479e9e30c218943f54e1f10134c63d63a0f6550
Edalb <70691111+Edalb@users.noreply.github.com>
2021-07-14 20:26:15 +0200

    HeroCard - sample code
    This part of code is responsible for filling and clearing cards in Steel and Magic's hero selection scene.

---
+++ SteelAndMagic/HeroCard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HeroCard : MonoBehaviour
+{
+    public int heroPosition;
+    public bool isFilled = false;
+    [SerializeField] Image characterImage;
+    [SerializeField] TextMeshProUGUI stackSize;
+    [SerializeField] FadeTween[] fadeTweens;
+    public CharacterAttributes loadedHeroData;
+    static StorageUI storage;
+
+    [SerializeField] CurrentHeroes currentHeroes;
+    void Start()
+    {
+        storage = FindObjectOfType<StorageUI>();
+        if(currentHeroes.selectedHeroes != null)
+        {
+            foreach (CharacterAttributes hero in currentHeroes.selectedHeroes)
+            {
+                if (hero.startPosition == heroPosition)
+                {
+                    FillCard(hero, false);
+                }
+            }
+        }
+    }
+    public void FillCard(CharacterAttributes hero, bool isClicked)
+    {
+        isFilled = true;
+        loadedHeroData = hero;
+        loadedHeroData.startPosition = heroPosition;
+        characterImage.sprite = loadedHeroData.heroSprite;
+        stackSize.text = loadedHeroData.stack.ToString();
+        if (isClicked)
+        {
+            storage.heroesBar.selectedHeroes.Add(loadedHeroData);
+        }   
+        storage.ManageStartButton();
+        foreach (FadeTween fadingObject in fadeTweens)
+        {
+            fadingObject.Appear();
+        }
+    }
+    public void CleanCard()
+    {
+        if (isFilled)
+        {
+            isFilled = false;
+            storage.SetIconReady(loadedHeroData);
+            loadedHeroData.startPosition = 0;
+            storage.heroesBar.selectedHeroes.Remove(loadedHeroData);
+            loadedHeroData.isDeployed = false;
+            loadedHeroData = null;
+            storage.ManageStartButton();
+            foreach (FadeTween fadingObject in fadeTweens)
+            {
+                fadingObject.Disappear();
+            }
+        }
+    }
+}
